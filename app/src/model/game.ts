5f6b04043047b388bdcc38374cb837a8ef08251c
河田鷹介 <benefium@gmail.com>
2021-12-14 19:53:22 +0900

    bugfix: 「そこに置いたら何個並びになるか」が隙間のある時に正しい表示にならない

--- app/src/model/game.ts
+++ app/src/model/game.ts
@@ -34,47 +34,68 @@ export namespace Game {
     playerFor: Player
   ) {
     const playerNotFor: Player = counterPlayer(playerFor);
+    const dirUp = 0;
+    const dirDown = 1;
+    const dirLeft = 2;
+    const dirRight = 3;
+    const dirUL = 4;
+    const dirDR = 5;
+    const dirUR = 6;
+    const dirDL = 7;
+    const tables = [
+      dirUp, dirDown, dirLeft, dirRight,
+      dirUL, dirDR, dirUR, dirDL,
+    ].map(() => {
+      return _.range(Row).map((i) => {
+        return _.range(Col).map((j) => 0);
+      });
+    });
+    // 「行き」のDP
+    for (let i = 0; i < Row; i += 1) {
+      for (let j = 0; j < Col; j += 1) {
+        if (exBoard[i][j] === playerNotFor) { continue; }
+        if (0 < i && exBoard[i - 1][j] === playerFor) {
+            tables[dirUp][i][j] = tables[dirUp][i - 1][j] + 1;
+        }
+        if (0 < j && exBoard[i][j - 1] === playerFor) {
+            tables[dirLeft][i][j] = tables[dirLeft][i][j - 1] + 1;
+        }
+        if (0 < i && 0 < j && exBoard[i - 1][j - 1] === playerFor) {
+            tables[dirUL][i][j] = tables[dirUL][i - 1][j - 1] + 1;
+        }
+        if (0 < i && j < Col - 1 && exBoard[i - 1][j + 1] === playerFor) {
+            tables[dirUR][i][j] = tables[dirUR][i - 1][j + 1] + 1;
+        }
+      }
+    }
+    // 「帰り」のDP
+    for (let i = Row - 1; 0 <= i; i -= 1) {
+      for (let j = Col - 1; 0 <= j; j -= 1) {
+        if (exBoard[i][j] === playerNotFor) { continue; }
+        if (i < Row - 1 && exBoard[i + 1][j] === playerFor) {
+            tables[dirDown][i][j] = tables[dirDown][i + 1][j] + 1;
+        }
+        if (j < Col - 1 && exBoard[i][j + 1] === playerFor) {
+            tables[dirRight][i][j] = tables[dirRight][i][j + 1] + 1;
+        }
+        if (i < Row - 1 && j < Col - 1 && exBoard[i + 1][j + 1] === playerFor) {
+            tables[dirDR][i][j] = tables[dirDR][i + 1][j + 1] + 1;
+        }
+        if (i < Row - 1 && 0 <= j && exBoard[i + 1][j - 1] === playerFor) {
+            tables[dirDL][i][j] = tables[dirDL][i + 1][j - 1] + 1;
+        }
+      }
+    }
+
     return _.range(Game.Row).map((i) => {
       return _.range(Game.Col).map((j) => {
-        let ans = 0;
-        if (exBoard[i][j] === playerNotFor) { return ans; }
-        let k;
-        // よこ
-        for (k = 1; j + k < Game.Col; k += 1) {
-          if (exBoard[i][j + k] !== playerFor) { break; }
-        }
-        if (k > ans) { ans = k; }
-        for (k = 1; 0 <= j - k; k += 1) {
-          if (exBoard[i][j - k] !== playerFor) { break; }
-        }
-        if (k > ans) { ans = k; }
-        // たて
-        for (k = 1; i + k < Game.Row; k += 1) {
-          if (exBoard[i + k][j] !== playerFor) { break; }
-        }
-        if (k > ans) { ans = k; }
-        for (k = 1; 0 <= i - k; k += 1) {
-          if (exBoard[i - k][j] !== playerFor) { break; }
-        }
-        if (k > ans) { ans = k; }
-        // ななめ
-        for (k = 1; i + k < Game.Row && j + k < Game.Col; k += 1) {
-          if (exBoard[i + k][j + k] !== playerFor) { break; }
-        }
-        if (k > ans) { ans = k; }
-        for (k = 1; 0 <= i - k && j + k < Game.Col; k += 1) {
-          if (exBoard[i - k][j + k] !== playerFor) { break; }
-        }
-        if (k > ans) { ans = k; }
-        for (k = 1; i + k < Game.Row && 0 <= j - k; k += 1) {
-          if (exBoard[i + k][j - k] !== playerFor) { break; }
-        }
-        if (k > ans) { ans = k; }
-        for (k = 1; 0 <= i - k && 0 <= j - k; k += 1) {
-          if (exBoard[i - k][j - k] !== playerFor) { break; }
-        }
-        if (k > ans) { ans = k; }
-        return ans;
+        if (exBoard[i][j] === playerNotFor) { return 0; }
+        return Math.max(
+          tables[dirUp][i][j] + tables[dirDown][i][j],
+          tables[dirLeft][i][j] + tables[dirRight][i][j],
+          tables[dirUL][i][j] + tables[dirDR][i][j],
+          tables[dirUR][i][j] + tables[dirDL][i][j],
+        ) + 1;
       });
     });
   }
