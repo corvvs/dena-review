3dea89d9c4a9835d138737e3073c64c00dea85e0
河田鷹介 <benefium@gmail.com>
2021-12-17 10:59:55 +0900

    マッチ同時リトライ

--- app/src/model/match.ts
+++ app/src/model/match.ts
@@ -1,10 +1,11 @@
 import _ from 'lodash';
 import * as FS from "firebase/firestore";
-import { v4 } from 'uuid';
-import { M4Player } from '../model/player'
-import { Game } from '../model/game'
-import { collection, doc, getDoc, getDocs, updateDoc, where } from 'firebase/firestore';
+import * as FSUtil from '../utils/firestore';
+import { M4Player } from '../model/player';
+import { Game } from '../model/game';
 
+const prolong = 60 * 1000;
+const timeout = 5 * 1000;
 
 // 「対戦」オブジェクト
 
@@ -24,6 +25,11 @@ type MatchOpened = {
    * 募集完了対戦オブジェクトのID; 候補が決まったら募集側が作る
    */
   closed_match_d?: string;
+  /**
+   * 対戦の有効期限
+   * 募集側が生きていれば延長される
+   */
+  expires_at: Date;
 };
 
 /**
@@ -39,7 +45,14 @@ type MatchClosed = {
   opponent_id: string;
   opponent_name: string
   logs: Game.Log[];
+  /**
+   * 対戦の有効期限
+   * どちらかが生きていれば延長される
+   */
+  expires_at: Date;
 };
+
+
 
 export namespace M4Match {
   const ColClosed = "match_closed";
@@ -50,6 +63,7 @@ export namespace M4Match {
       created_at: new Date(),
       registerer_id: player.id,
       registerer_name: player.name,
+      expires_at: new Date(Date.now() + prolong),
     };
   }
 
@@ -65,6 +79,7 @@ export namespace M4Match {
       opponent_id,
       opponent_name,
       logs: [],
+      expires_at: new Date(Date.now() + prolong),
     };
   }
 
@@ -72,100 +87,111 @@ export namespace M4Match {
     // `match_opened`にドキュメントがないか探す。
 
     const db = FS.getFirestore();
-    const q = FS.query(FS.collection(db, ColOpened));
-    const result = await FS.getDocs(q);
-    console.log(result.docs);
-    const opened_docs = result.docs.filter(d => !d.get("opponent_id"));
+    const q = FS.query(
+      FS.collection(db, ColOpened),
+      FS.where("expires_at", ">", new Date()),
+    );
+    const maxRetry = 1000;
+    for (let i = 0; i < maxRetry; i += 1) {
+      try {
+        console.log(`matching try #${i}`);
+        const result = await FS.getDocs(q);
+        const opened_docs = result.docs.filter(d => !d.get("opponent_id"));
 
-    if (opened_docs.length === 0) {
-      // [ドキュメントがない場合]
-      console.log("no opened doc");
-      // `match_opened`にドキュメントを作成し、listenする。
-      let opponent_id: string | null = null;
-      let opponent_name: string | null = null;
-      const matchOpened = makeOpenedMatch(player);
-      const matchOpenedRef = await FS.addDoc(FS.collection(db, ColOpened), matchOpened);
-      console.log(`made opened doc: ${matchOpenedRef.id}`);
-      const matchClosedRef = await (new Promise<FS.DocumentReference<FS.DocumentData>>((res, rej) => {
-        let rejected = false;
-        const unsubscriber = FS.onSnapshot(matchOpenedRef, {
-          next: async (snapshot) => {
-            if (rejected) { return; }
-            if (!snapshot.exists) { rejected = true; unsubscriber(); rej("deleted"); return; }
-            // `match_opened`ドキュメントの`opponent_id`に値が入った場合、
-            opponent_id = snapshot.get("opponent_id");
-            if (!opponent_id) { return; }
-            opponent_name = snapshot.get("opponent_name") || "";
-            console.log(`opponent_id is: ${opponent_id}`);
-            
-            // 1. `match_closed`ドキュメントを作成
-            const matchClosed = makeClosedMatch(matchOpened, opponent_id, opponent_name!);
-            const matchClosedRef = await FS.addDoc(FS.collection(db, ColClosed), matchClosed);
-            console.log(`made closed doc: ${matchClosedRef.id}`);
-            // 2. `match_opened`ドキュメントのlistenを解除
-            unsubscriber();
-            res(matchClosedRef);
-          },
-        });
-        console.log(`listening opened doc: ${matchOpenedRef.id}`);
-      }));
-      // 3. `match_opened`ドキュメントの`closed_match_id`に`match_closed`ドキュメントのIDを書き込む
-      await (new Promise(async (res, rej) => {
-        let rejected = false;
-        const unsubscriber = FS.onSnapshot(matchClosedRef, {
-          next: async (snapshot) => {
-            if (rejected) { return; }
-            if (!snapshot.exists) { rejected = true; unsubscriber(); rej("deleted"); return; }
-            // 4. `match_closed`ドキュメントの`opponent_id`に`match_opened`ドキュメントの`opponend_id`が入ったら、`match_opened`ドキュメントを削除
-            const closed_opponent_id = snapshot.get("opponent_id");
-            if (!closed_opponent_id) { return; }
-            console.log(`receipt id: ${closed_opponent_id}`);
-            console.log(`matched up`);
-            await FS.deleteDoc(matchOpenedRef);
-            unsubscriber();
-            res("ok");
-          },
-        });
-        console.log(`listening closed doc: ${matchClosedRef.id}`);
-        await FS.updateDoc(matchOpenedRef, { closed_match_id: matchClosedRef.id });
-        console.log(`write closed_match_id into opened doc: ${matchClosedRef.id}`);
-      }));
-      
-      // 5. ゲーム開始
-      return Game.init2pGame(
-        matchClosedRef.id,
-        player,
-        {
-          id: opponent_id!,
-          name: opponent_name!
-        },
-        true,
-      );
+        if (opened_docs.length === 0) {
+          return await getMatchSupply(player);
+        } else {
+          return await getMatchDemand(player, opened_docs[0]);
+        }
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    throw new Error("matching error");
+  }
 
-    } else {
+  async function getMatchSupply(player: M4Player.PlayerData) {
+    const db = FS.getFirestore();
+    // [ドキュメントがない場合]
+    console.log("no opened doc");
+    // `match_opened`にドキュメントを作成し、listenする。
+    let opponent_id: string | null = null;
+    const matchOpened = makeOpenedMatch(player);
+    const matchOpenedRef = await FS.addDoc(FS.collection(db, ColOpened), matchOpened);
+    console.log(`made opened doc: ${matchOpenedRef.id}`);
+
+
+    const matchClosed = await FSUtil.askFirstUpdate(
+      matchOpenedRef,
+      (snapshot) => {
+        if (!snapshot.exists) { return "reject"; }
+        opponent_id = snapshot.get("opponent_id");
+        if (!opponent_id) { return "ignore"; }
+        return "accept";
+      },
+      (snapshot) => {
+          const { opponent_id, opponent_name } = snapshot.data()!;
+          const matchClosed = makeClosedMatch(matchOpened, opponent_id!, opponent_name!);
+          return matchClosed;
+      },
+      { timeout },
+    )
+    // 1. `match_closed`ドキュメントを作成
+    const matchClosedRef = await FS.addDoc(FS.collection(db, ColClosed), matchClosed);
+    console.log(`made closed doc: ${matchClosedRef.id}`);
+
+    // 3. `match_opened`ドキュメントの`closed_match_id`に`match_closed`ドキュメントのIDを書き込む
+    await FS.updateDoc(matchOpenedRef, { closed_match_id: matchClosedRef.id });
+    // 4. `match_closed`ドキュメントの`opponent_id`に`match_opened`ドキュメントの`opponend_id`が入ったら、`match_opened`ドキュメントを削除
+    const { opponent_id: closed_opponent_id, opponent_name } = await FSUtil.askFirstUpdate(
+      matchClosedRef,
+      (snapshot) => {
+        if (!snapshot.exists) { return "reject"; }
+        const closed_opponent_id = snapshot.get("opponent_id");
+        if (!closed_opponent_id) { return "ignore"; }
+        return "accept";
+      },
+      (snapshot) => snapshot.data()!,
+      { timeout },
+    );
+    console.log(`receipt id: ${closed_opponent_id}`);
+    console.log(`matched up`);
+    await FS.deleteDoc(matchOpenedRef);
+    
+    // 5. ゲーム開始
+    return Game.init2pGame(
+      matchClosedRef.id,
+      player,
+      {
+        id: opponent_id!,
+        name: opponent_name!
+      },
+      true,
+    );
+  }
+
+  async function getMatchDemand(player: M4Player.PlayerData, openedDoc: FS.QueryDocumentSnapshot<FS.DocumentData>) {
       // [ドキュメントがある場合]
-      const openedDoc = opened_docs[0];
+      const db = FS.getFirestore();
       console.log(`found opened doc: ${openedDoc.id}`);
-      const closed_match_id = await (new Promise<string>(async (res, rej) => {
-        let rejected = false;
-        // 1. `match_opened`ドキュメントに対して自分のIDを書き込む。
-        await updateDoc(openedDoc.ref, { opponent_id: player.id, opponent_name: player.name });
-        // 2. `match_opened`ドキュメントをlisten
-        const unsubscriber = FS.onSnapshot(openedDoc.ref, {
-          next: async (snapshot) => {
-            if (rejected) { return; }
-            if (!snapshot.exists) { rejected = true; unsubscriber(); rej("deleted"); return; }
-            // 3. `match_opened`ドキュメントの`closed_match_id`に値が入ったら、対応する`match_closed`ドキュメントを見に行く。
-            const closed_match_id = snapshot.get("closed_match_id");
-            if (!closed_match_id) { return; }
-            console.log(`receipt id: ${closed_match_id}`);
-            // 4. `match_opened`ドキュメントのlistenを解除
-            unsubscriber();
-            res(closed_match_id);
-          },
-        });
-        console.log(`listening opened doc: ${openedDoc.id}`);
-      }));
+
+      // 1. `match_opened`ドキュメントに対して自分のIDを書き込む。
+      await FS.updateDoc(openedDoc.ref, { opponent_id: player.id, opponent_name: player.name });
+      // 2. `match_opened`ドキュメントをlisten
+      const closed_match_id = await FSUtil.askFirstUpdate(
+        openedDoc.ref,
+        (snapshot) => {
+          if (!snapshot.exists) { return "reject"; }
+          const closed_match_id = snapshot.get("closed_match_id");
+          if (!closed_match_id) { return "ignore"; }
+          return "accept";
+        },
+        (snapshot) => snapshot.get("closed_match_id")!,
+        { timeout },
+        );
+      // 3. `match_opened`ドキュメントの`closed_match_id`に値が入ったら、対応する`match_closed`ドキュメントを見に行く。
+      console.log(`receipt match id: ${closed_match_id}`);
+
       // 5. `match_closed`ドキュメントの`opponent_id`が自分と同じだったら、ゲーム開始
       const closedMatch = await FS.getDoc(FS.doc(db, ColClosed, closed_match_id));
       const { registerer_id, registerer_name, opponent_id } = closedMatch.data() || {};
@@ -182,16 +208,5 @@ export namespace M4Match {
         },
         false,
       );
-
-    }
-
-  }
-
-  export async function openMatch(player: M4Player.PlayerData) {
-    // const db = FS.getFirestore();
-    // const ref = await FS.addDoc(FS.collection(db, ColOpened), {
-    //   test: "ok",
-    // });
-    return null;;
   }
 };
